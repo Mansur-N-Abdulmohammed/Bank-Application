4597a64407bb2da5212a58dc54fd1ac4bb788118
MansurSharazoori <Xpert.Web.911@gmail.com>
2026-01-28 13:57:02 +0300

    working on Dates

--- script.js
+++ script.js
@@ -37,10 +37,22 @@ let currentSorted = false;
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
-  type: 'premium',
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -48,7 +60,19 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
-  type: 'basic',
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -84,11 +108,15 @@ const dispMovements = function (mov, sort = false) {
         <div class="movements__type movements__type--${type}">
         ${i + 1} ${type}</div>
          <div class="movements__date">3 days ago</div>
-         <div class="movements__value">${Math.abs(e)}</div>
+         <div class="movements__value">${Math.abs(e).toFixed(2)}$</div>
          </div>
          `;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+  document.querySelectorAll('.movements__row').forEach((row, i) => {
+    if (i % 2 == 0) row.style.backgroundColor = 'black';
+    if (i % 2 == 1) row.style.backgroundColor = 'grey';
   });
 };
 
@@ -136,14 +164,12 @@ const summeryAccount = function (acc) {
     const income = acc.movements
       .filter(e => e > 0)
       .map(e => e * 1)
-      .reduce((a, b) => a + b, 0)
-      .toFixed(2);
+      .reduce((a, b) => a + b, 0);
 
     const outcome = acc.movements
       .filter(e => e < 0)
       .map(e => e * 1)
-      .reduce((a, b) => a + b, 0)
-      .toFixed(2);
+      .reduce((a, b) => a + b, 0);
 
     const intersect = acc.movements
       .filter(e => e > 0)
@@ -155,8 +181,8 @@ const summeryAccount = function (acc) {
     // labelSumInterest.textContent = `${intersect}$`;
 
     labelSumInterest.textContent = `ولا تقرب الربا`;
-    labelSumIn.textContent = `${income}$`;
-    labelSumOut.textContent = `${Math.abs(outcome)}$`;
+    labelSumIn.textContent = `${income.toFixed(2)}$`;
+    labelSumOut.textContent = `${Math.abs(outcome).toFixed(2)}$`;
   } else {
     labelSumIn.textContent = `0000$`;
   }
@@ -291,13 +317,3 @@ UpdateAccount(account1);
 //
 //
 // /////////////////////////////////////////// testingarea
-const users = [
-  { name: 'Ali', role: 'admin' },
-  { name: 'Sara', role: 'user' },
-  { name: 'Omar', role: 'user' },
-  { name: 'Lina', role: 'admin' },
-  { name: 'Yusuf', role: 'guest' },
-];
-
-// const keys = Object.groupBy(users, e => e.role);
-// console.log(keys);
